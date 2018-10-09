58e8f4119c4194ee1e5534da6026813e7497ed27
simanim <42598177+simanim@users.noreply.github.com>
2018-10-09 19:41:36 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo:"login", pathMatch: "full"}
 ];
 
